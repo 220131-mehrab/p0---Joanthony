6fcf07e7b28ed920a00ee7bd75d475847b78a08a
jojom <jojomcgee03@gmail.com>
2022-02-17 14:51:13 -0600

    making changes - adding new classes, introducing HTML, introducing server/servlet.

--- src/main/java/com/revature/pokedex/App.java
+++ src/main/java/com/revature/pokedex/App.java
@@ -1,5 +1,8 @@
 package com.revature.pokedex;
 
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.startup.Tomcat;
+
 public class App {
     //location where we do business logic and gather content.
     public static void main(String[] args) {
@@ -8,7 +11,19 @@ public class App {
 
         DexRepository dexRepository = new DexRepository("pokedex.csv");
         DexService dexService = new DexService(dexRepository);
-        DexServer dexServer = new DexServer(dexService);
+        SearchForService sfService = new SearchForService();
 
+        Tomcat server = new Tomcat();
+        server.setBaseDir(System.getProperty("java.io.tmpdir"));
+        //server.setPort(0);
+        server.getConnector();
+        server.addContext("", null);
+        server.addServlet("", "dexServlet", dexService).addMapping("/pokemon");
+        server.addServlet("", "searchFormServlet", sfService).addMapping("/search");
+        try {
+            server.start();
+        } catch (LifecycleException e) {
+            e.printStackTrace();
+        }
     }
 }
